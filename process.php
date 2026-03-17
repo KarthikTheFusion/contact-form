2164247b0061b5c8c92ea5f70ce793f711dc5795
SAMULA KARTHIK <119867909+KarthikTheFusion@users.noreply.github.com>
2026-03-17 22:32:19 +0530

    Add files via upload

---
+++ process.php
@@ -0,0 +1,13 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    echo "<h2>Form Submitted Successfully!</h2>";
+    echo "Name: " . $name . "<br>";
+    echo "Email: " . $email . "<br>";
+    echo "Message: " . $message;
+}
+?>
